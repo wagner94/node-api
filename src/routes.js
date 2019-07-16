45dcf12b0863f233c000badb7f37c938923b57fb
van <evandertech@hotmail.com>
2019-07-16 14:17:25 +0100

    Fase Inicial do Projecto

---
+++ src/routes.js
@@ -0,0 +1,12 @@
+//Primeira rota
+const express = require("express");
+const routes = express.Router();
+const ProductController = require('./controllers/ProductController');
+
+routes.get('/products',ProductController.index);
+routes.get('/products/:id',ProductController.show);
+routes.post('/products',ProductController.store);
+routes.put('/products',ProductController.update);
+routes.delete('/products/:id',ProductController.destroy);
+
+  module.exports = routes;
